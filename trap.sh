727ec99de90cf2d85d1403a7d644be5b1409371b
venkat-5555 <venkatsaikrishna5555@gmail.com>
2025-03-26 10:17:41 +0530

    set

---
+++ trap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#set -ex
+failure(){
+    echo "the error is at : $1 and command is $2"
+}
+trap 'failure "$LINENO" "$BASH_COMMAND"' ERR
+echo "hi , hello"
+echhoo "how r u?"
+echo "bye"
+pas
+pwd
+ls -l
