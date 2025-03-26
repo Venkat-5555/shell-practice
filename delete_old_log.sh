883db49ceb64150d31a07800868260c8e49a2085
venkat-5555 <venkatsaikrishna5555@gmail.com>
2025-03-26 18:31:31 +0530

    set

--- delete_old_log.sh
+++ delete_old_log.sh
@@ -7,7 +7,7 @@ else
     echo "directory dosent exists"
     exit 1
 fi
-files=$(find $SOURCE -name "*.log" -mtime +20)
+files=$(find $SOURCE -name "*.log")
 echo "Files: $files"
 while IFS= read -r file  #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /,*,$, line is reserved key word
 do
