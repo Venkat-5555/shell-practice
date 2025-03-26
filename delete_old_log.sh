bf56af7c3ff59e3c5f3bac53ba0fb1036ec7354f
venkat-5555 <venkatsaikrishna5555@gmail.com>
2025-03-26 18:27:02 +0530

    set

--- delete_old_log.sh
+++ delete_old_log.sh
@@ -1,5 +1,5 @@
 SOURCE=/home/ec2-user/logs
-files=$(find $SOURCE -name "*.log" -mtime +20)
+
 if [ -d $SOURCE ]
 then  
     echo "directory exists"
@@ -7,7 +7,7 @@ else
     echo "directory dosent exists"
     exit 1
 fi
-
+files=$(find $SOURCE -name "*.log" -mtime +20)
 echo "Files: $files"
 while IFS= read -r file  #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /,*,$, line is reserved key word
 do
