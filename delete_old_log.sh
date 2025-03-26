1b50c200175eb0b3e59a4f7da68d42542ee79273
venkat-5555 <venkatsaikrishna5555@gmail.com>
2025-03-26 18:23:17 +0530

    set

--- delete_old_log.sh
+++ delete_old_log.sh
@@ -12,5 +12,5 @@ echo "Files: $files"
 while IFS= read -r file  #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /,*,$, line is reserved key word
 do
     echo "deleting file: $file"
-    rm -rf $line
+    rm -rf $file
 done <<< $files
